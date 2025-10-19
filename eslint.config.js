12d449acfd8e193373a5c5bf2084ca131f3f63d1
Shirley <chowjss@gmail.com>
2025-10-19 15:58:32 +0800

    Add lint, unit tests, and dependency check

---
+++ eslint.config.js
@@ -0,0 +1,18 @@
+import pluginVue from "eslint-plugin-vue";
+import globals from "globals";
+
+export default [
+  // add more generic rulesets here, such as:
+  // js.configs.recommended,
+  ...pluginVue.configs["flat/recommended"],
+  // ...pluginVue.configs['flat/vue2-recommended'], // Use this if you are using Vue.js 2.x.
+  {
+    rules: {},
+    languageOptions: {
+      sourceType: "module",
+      globals: {
+        ...globals.browser,
+      },
+    },
+  },
+];
